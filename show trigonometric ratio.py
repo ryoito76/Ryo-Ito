946054a4135ea3183e04c1592fa95303844d856f
ryoito76 <ryo.ito@tokyo.supersoftware.co.jp>
2024-04-12 13:34:07 +0900

    Pyhton 三角比表示

---
+++ show trigonometric ratio.py
@@ -0,0 +1,18 @@
+import math
+result=[]
+for i in range(0,195,15):
+        result.append(i)
+        sini= math.sin(math.radians(i))
+        print('sin'+str(i)+'='+str(sini))
+result=[]
+for i in range(0,195,15):
+        result.append(i)
+        cosi= math.cos(math.radians(i))
+        print('cos'+str(i)+'='+str(cosi))
+result=[]
+for i in range(0,195,15):
+        result.append(i)
+        tani= math.tan(math.radians(i))
+        print('tan'+str(i)+'='+str(tani))
+
+
